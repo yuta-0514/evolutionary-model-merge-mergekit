bf1d799bc776f0ed62b8dd028f345a08995798dd
yuta-0514 <y.akutsu0514@gmail.com>
2024-12-22 12:32:07 +0900

    Add Chat Vector

--- chat_vector/chat_vector.py
+++ chat_vector/chat_vector.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import torch
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -8,42 +9,50 @@ load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 
-if __name__ == "__main__":
-    eng_model = AutoModelForCausalLM.from_pretrained(
-        "mistralai/Mistral-7B-v0.1",
+def main(args):
+    base_model = AutoModelForCausalLM.from_pretrained(
+        args.base_model,
         torch_dtype=torch.bfloat16,
         device_map="cpu",
         token=HF_TOKEN
     )
-    eng_tokenizer = AutoTokenizer.from_pretrained(
-        "mistralai/Mistral-7B-v0.1"
+    base_tokenizer = AutoTokenizer.from_pretrained(
+        args.base_model
     )
-    math_model = AutoModelForCausalLM.from_pretrained(
-        "WizardLMTeam/WizardMath-7B-V1.1",
+    skilled_model = AutoModelForCausalLM.from_pretrained(
+        args.skilled_model,
         torch_dtype=torch.bfloat16,
         device_map="cpu",
     )
-    jp_model = AutoModelForCausalLM.from_pretrained(
-        "augmxnt/shisa-gamma-7b-v1",
+    cp_model = AutoModelForCausalLM.from_pretrained(
+        args.cp_model,
         torch_dtype=torch.bfloat16,
         device_map="cuda",
     )
-    jp_tokenizer = AutoTokenizer.from_pretrained(
-        'augmxnt/shisa-gamma-7b-v1'
+    cp_tokenizer = AutoTokenizer.from_pretrained(
+        args.cp_model
     )
 
-    if jp_tokenizer.vocab_size == eng_tokenizer.vocab_size:
-        # 除外対象
+    if cp_tokenizer.vocab_size == base_tokenizer.vocab_size:
+        # Excluded　module
         skip_layers = []
     else:
         exit(1)
-    for k, v in jp_model.state_dict().items():
+    for k, v in cp_model.state_dict().items():
         if (k in skip_layers) or ("layernorm" in k):
             continue
-        chat_vector = math_model.state_dict()[k] - eng_model.state_dict()[k]
+        chat_vector = skilled_model.state_dict()[k] - base_model.state_dict()[k]
         new_v = v + chat_vector.to(v.device)
         v.copy_(new_v)
-    new_model_name = 'chat_vector_mistral_7B_math'
-    jp_model.save_pretrained(new_model_name)
-    jp_tokenizer.save_pretrained(new_model_name)
-# MGSM：0.352
+    cp_model.save_pretrained(args.new_model_name)
+    cp_tokenizer.save_pretrained(args.new_model_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create a new model using Chat Vector.')
+    parser.add_argument("--base_model", type=str, default="mistralai/Mistral-7B-v0.1", help="Base model name")
+    parser.add_argument("--skilled_model", type=str, default="WizardLMTeam/WizardMath-7B-V1.1", help="Skilled model name")
+    parser.add_argument("--cp_model", type=str, default="augmxnt/shisa-gamma-7b-v1", help="CP model name")
+    parser.add_argument("--new_model", type=str, default="chat_vector_mistral_7B_math", help="New model name")
+    args = parser.parse_args()
+    main(args)
